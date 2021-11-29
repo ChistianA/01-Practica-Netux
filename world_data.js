c589bed03084fbb807096a94a5055c3ddbcd47cb
Christian Andrade <christian.andrade@upb.edu.co>
2021-11-29 18:21:44 -0500

    Table and Data Update

--- world_data.js
+++ world_data.js
@@ -5,13 +5,21 @@ fetch("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com
 		"x-rapidapi-key": "ddb752c21dmsh170f263d1321659p1fc6bcjsn578e083ac12d"
 	}
 })
-.then(response => response.json() )
-.then(response =>{
+.then(response => response.json())
+.then(data => {
+	let total_cases = document.getElementById('total_cases')
+	total_cases.innerHTML = `${data[0].TotalCases}`
+	let active_cases = document.getElementById('active_cases')
+	active_cases.innerHTML = `${data[0].ActiveCases}`
+	let total_deaths = document.getElementById('total_deaths')
+	total_deaths.innerHTML = `${data[0].TotalDeaths}`
+	let total_recovered = document.getElementById('total_recovered')
+	total_recovered.innerHTML = `${data[0].TotalRecovered}`
 
-    let element = document.getElementById('total_cases')
-    element.innerHTML = `<p>${response.ActiveCases}</p>`;
-	console.log(response);
+	console.log(data);
 })
-.catch(err => {
-	console.error(err);
-});
+
+.catch(err => console.log(err))
+
+
+
